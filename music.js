bcdb0e6f5c7984aebf3b6082841e85ee117c7f48
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2018-08-09 20:54:08 +0300

    Update music.js

--- music.js
+++ music.js
@@ -8,7 +8,6 @@ const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '!';
-const discord_token = "";
 client.login(discord_token);
 client.on('ready', function() {
 	console.log(`i am ready ${client.user.username}`);
